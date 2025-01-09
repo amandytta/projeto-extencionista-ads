a1bc5ce785b98bad8732d175859bcddd8bf64758
Gabriel <gabrielmartineli88@Gmail.com>
2025-01-08 21:39:37 -0300

    mudar icone qnd abrir e fechar menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-const toggleButton = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links a');
 
-toggleButton.addEventListener('click', () => {
+
+menu.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+  menu.classList.toggle('open');
+});
+
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target)) {
+    navLinks.classList.remove('active');
+    menu.classList.remove('open');
+  }
 });
 
 links.forEach(link => {
   link.addEventListener('click', () => {
     navLinks.classList.remove('active');
+    menu.classList.remove('open');
   });
 });
