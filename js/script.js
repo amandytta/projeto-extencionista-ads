bdb85c983da7a4568d4849351d927bb2a3d44e7b
Gabriel <gabrielmartineli88@Gmail.com>
2025-12-14 21:14:48 -0300

    dark-mode adicionado no site na versão PC

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const menu = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links a');
+const imgTheme = document.getElementById('change-theme');
 
+imgTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode')
+    
+
+    const darkVerify = document.body.classList.contains('dark-mode')
+
+    if(darkVerify) {
+      imgTheme.src = './assets/imagens/eclipse-alt.png'
+    } else {
+      imgTheme.src = './assets/imagens/estrelas-da-lua.png'
+    }
+})
 
 menu.addEventListener('click', () => {
   navLinks.classList.toggle('active');
